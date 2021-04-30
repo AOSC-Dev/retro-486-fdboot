b0924e7fae669892022a9dbfb0bde2907b769bf7
Neo_Chen <chenkolei@gmail.com>
2021-04-30 21:41:51 +0800

    Added mkiso.sh

---
+++ scripts/mkiso.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rsync -avr cd work
+cp -v bin/busybox work/cd
+rsync -avr --del linux/tar-install/lib/modules work/cd/lib
+(
+	cd work/cd
+	bsdtar -xpvvf skel.tar.gz
+	rm -v skel.tar.gz
+)
+
+[ ! -d img ] && mkdir img
+
+mkisofs -R -r -V AOSC_INSTCD -o img/aosc.iso work/cd
