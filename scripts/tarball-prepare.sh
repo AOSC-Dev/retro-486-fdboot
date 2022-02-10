95ed03dbcc24952eccd9ab67c2a6b88cb3e2ea72
Neo_Chen <chenkolei@gmail.com>
2022-02-10 19:25:08 +0800

    tarball-prepare: no eval, no evil

--- scripts/tarball-prepare.sh
+++ scripts/tarball-prepare.sh
@@ -5,9 +5,9 @@ mkdir -p tarball
 cd tarball
 
 BASEURL='https://releases.aosc.io/os-i486/minimal/'
-eval `curl "$BASEURL" | grep 'href.*i486.tar.xz<' | cut -d '>' -f 1 | cut -d ' ' -f 2`
+FILE="$(curl "$BASEURL" | grep 'href.*i486.tar.xz<' | cut -d '"' -f 2)"
 
-curl -OJ "$BASEURL"/"$href"
+curl -OJ "$BASEURL"/"$FILE"
 
 for i in *.xz ; do
 	xz -dvv "$i"
