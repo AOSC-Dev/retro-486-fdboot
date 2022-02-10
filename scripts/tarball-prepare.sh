4a3d889429df72e745907fe0e0517b19e71b89bf
Neo_Chen <chenkolei@gmail.com>
2022-02-10 21:41:57 +0800

    tarball-prepare: use grep -o to eliminate the need for cut

--- scripts/tarball-prepare.sh
+++ scripts/tarball-prepare.sh
@@ -3,9 +3,10 @@
 
 mkdir -p tarball
 cd tarball
+rm -fv *.tar
 
 BASEURL='https://releases.aosc.io/os-i486/minimal/'
-FILE="$(curl "$BASEURL" | grep 'href.*i486.tar.xz<' | cut -d '"' -f 2)"
+FILE="$(curl "$BASEURL"  | grep -o 'aosc-os_minimal_[0-9]*_i486.tar.xz' | tail -n 1)"
 
 curl -OJ "$BASEURL"/"$FILE"
 
