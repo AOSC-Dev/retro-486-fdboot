3c0b42154d4b023ba2508292f1fc10d8a58459f9
Neo_Chen <chenkolei@gmail.com>
2022-02-12 18:08:03 +0800

    tarball-prepare: use rsync

--- scripts/tarball-prepare.sh
+++ scripts/tarball-prepare.sh
@@ -5,10 +5,10 @@ mkdir -p tarball
 cd tarball
 rm -fv *.tar
 
-BASEURL='https://releases.aosc.io/os-i486/minimal/'
-FILE="$(curl "$BASEURL"  | grep -o 'aosc-os_minimal_[0-9]*_i486.tar.xz' | tail -n 1)"
+BASEURL='rsync://releases.aosc.io/releases/os-i486/minimal/'
+FILE="$(rsync --exclude=rawimg --no-motd "$BASEURL"  | grep -o 'aosc-os_minimal_[0-9]*_i486.tar.xz' | tail -n 1)"
 
-curl -OJ "$BASEURL"/"$FILE"
+rsync -vP "$BASEURL"/"$FILE" .
 
 for i in *.xz ; do
 	xz -dvv "$i"
