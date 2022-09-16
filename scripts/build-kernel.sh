db0aa573b01acc330bb1d5de3a3e53dfeae94d6f
Neo_Chen <chenkolei@gmail.com>
2022-09-16 11:28:16 +0800

    fixed scripting issue

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -4,6 +4,7 @@ rm -rf bin/lib
 
 cp -v config/linux.config linux/.config
 cd linux
+nice -n 20 make -j "$(nproc)" oldconfig
 nice -n 20 make -j "$(nproc)"
 make INSTALL_MOD_PATH="$OLDPWD/bin" INSTALL_MOD_STRIP=1 modules_install
 cp -v ./arch/x86/boot/bzImage "$OLDPWD"/bin/linux
--- scripts/common.sh
+++ scripts/common.sh
@@ -2,5 +2,8 @@ set -ex
 export OLDPWD="$PWD"
 export PATH="$PATH:$PWD/toolchain/bin"
 
-alias c='git clone --depth 1'
+function c()
+{
+	git clone --depth 1 "$1"
+}
 
